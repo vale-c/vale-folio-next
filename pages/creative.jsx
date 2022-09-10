b3a80506f9f6308581e6a8a6da53dd62f30e5940
Valentina <valent95@gmail.com>
2022-09-10 15:40:19 +0200

    latest fix Circle.js

--- pages/creative.jsx
+++ pages/creative.jsx
@@ -1,15 +1,15 @@
+import dynamic from 'next/dynamic';
 
-import dynamic from "next/dynamic";
+const Circle = dynamic(() => import('../components/CodeArt/Circle'), {
+  ssr: false
+});
 
-const Circle = dynamic(
-  import("../components/CodeArt/Circle"),
-  { ssr: false }
+const Creative = () => (
+  <div className="m-4">
+    <h1 className="text-3xl font-bold text-center mt-8">Creative Coding</h1>
+    <Circle />
+    <div className="p-2" />
+  </div>
 );
 
-export default () => {
-  return <div className="m-4">
-  <h1 className="text-3xl font-bold text-center mt-8">Creative Coding</h1>
-  <Circle />
-  <div className="p-2" />
-</div>;
-};
+export default Creative;
