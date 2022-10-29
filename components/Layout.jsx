5746ecf7a5dcd02a9d44322e434bc4fed6e6818f
Valentina Calabrese <valent95@gmail.com>
2022-10-29 21:17:22 +0200

    wrapping Layout with animation

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,6 +4,13 @@ import { Cursor } from './Cursor';
 import { LayoutGroup } from 'framer-motion';
 import { Footer } from './Footer';
 import { Loader } from './Loader';
+import { motion } from 'framer-motion';
+
+const variants = {
+  hidden: { opacity: 0, x: -200, y: 0 },
+  enter: { opacity: 1, x: 0, y: 0 },
+  exit: { opacity: 0, x: 0, y: -100 }
+};
 
 const Layout = ({ isLoading, ...props }) => {
   const containerRef = useRef(null);
@@ -12,12 +19,20 @@ const Layout = ({ isLoading, ...props }) => {
   }
   return (
     <LayoutGroup type="crossfade">
-      <div data-scroll-container className="w-screen" ref={containerRef}>
-        <NavHeader />
-        {props.children}
-      </div>
-      <Cursor />
-      <Footer />
+      <motion.main
+        initial="hidden"
+        animate="enter"
+        exit="exit"
+        variants={variants}
+        transition={{ type: 'linear' }}
+      >
+        <div data-scroll-container className="w-screen" ref={containerRef}>
+          <NavHeader />
+          {props.children}
+        </div>
+        <Cursor />
+        <Footer />
+      </motion.main>
     </LayoutGroup>
   );
 };
